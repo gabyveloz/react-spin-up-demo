a34044ec929cd140dd6deb71747cc3cbce5a1383
Gabriela Veloz <velozitywc@gmail.com>
2020-05-11 14:58:25 -0600

    created react demo with error files in them.

---
+++ app/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter} from "react-router-dom";
+import {Route, Switch} from "react-router";
+import {FourOhFour} from "./pages/FourOhFour";
+import {Home} from "./pages/Home";
+import {Other} from "./pages/Other";
+
+const Routing = () => (
+	<>
+		<h2>Navbar</h2>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Home}/>
+				<Route exact path="/Other" component={Other}/>
+				<Route component={FourOhFour}/>
+			</Switch>
+		</BrowserRouter>
+		<h3>Footer</h3>
+	</>
+);
+ReactDOM.render(<Routing/>, document.querySelector('#root'));
